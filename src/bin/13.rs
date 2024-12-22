6699cded020b62193d05d96b8bb11e491e367740
Xavier Morel <morelx42@gmail.com>
2024-12-22 11:46:09 +0100

    fix: fix test

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -151,6 +151,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(42));
+        assert_eq!(result, Some(459236326669));
     }
 }
